c481f9bcbde3e234b08960ee96ae17b05c38a296
Armel wanes <armelgeek5@gmail.com>
2022-10-10 15:57:54 +0200

    first commit
    first commit

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const path = require('path')
 
 function createWindow () {
  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
-const createWindow = () => {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
   win = new BrowserWindow({ width, height });
   win.loadURL("http://localhost:1200");
   win.focus();
